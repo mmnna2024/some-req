aef44301fa02b6683cf73ba6454e2495605ff8ce
naomu <muramatsu2023@gmail.com>
2024-04-03 19:02:16 +0900

    fix:add defalt false to display column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_03_023507) do
+ActiveRecord::Schema.define(version: 2024_04_03_095841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 2024_04_03_023507) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.integer "price", null: false
-    t.boolean "display", null: false
+    t.boolean "display", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
