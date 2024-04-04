6962cb836e86f59dfb9d9ea3ccfa6991e6be8917
kasagata <k.sagata.1207@gmail.com>
2024-04-04 16:57:34 +0900

    add: Add customer order Pages

---
+++ app/javascript/components/order/New.vue
@@ -0,0 +1,62 @@
+<script setup>
+import Form from './Form.vue'
+import Important from './Important.vue'
+import Confirm from './Confirm.vue'
+import { ref } from 'vue'
+
+
+
+const currentPageIndex = ref(0)
+
+const Pages = [
+  Form,
+  Important,
+  Confirm
+]
+
+function nextPage() {
+  if (currentPageIndex.value < Pages.length - 1) {
+  currentPageIndex.value = (currentPageIndex.value + 1) % Pages.length
+  }
+}
+
+function previousPage() {
+  if (currentPageIndex.value > 0) {
+  currentPageIndex.value = (currentPageIndex.value - 1) % Pages.length
+  }
+}
+
+
+</script>
+
+<template>
+  <div class="demo">
+    <component :is="Pages[currentPageIndex]" class="tab-Page" 
+    v-bind:categroies="categories"
+    ></component>
+
+    <button type="button" class="btn btn-outline-primary" @click="nextPage" v-if="currentPageIndex < Pages.length - 1">
+      Next Page
+    </button>
+    <button type="button" class="btn btn-outline-primary" @click="previousPage" v-if="currentPageIndex > 0">
+      Previous Page
+    </button>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.categories-table {
+  border: 1px solid gray;
+  margin: 10px;
+}
+
+.categories-table th,
+.categories-table td {
+  border: 1px solid gray;
+}
+</style>
+
