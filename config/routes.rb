dcf0ebf055fdb77baa0bf1189d1a6370f246d418
kasagata <k.sagata.1207@gmail.com>
2024-04-04 10:46:21 +0900

    vue test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'orders/new'
   devise_for :admins
   root 'hello_vue#index'
   get 'hello_vue/index'
-  
+  resources :orders
   
   #letter_openerを使用するためのルーティング
   if Rails.env.development?
