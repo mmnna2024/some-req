2c1cd180d68b6fa2fd785a960c42a065435bb3c5
kasagata <k.sagata.1207@gmail.com>
2024-04-05 19:15:22 +0900

    add: confirm page

--- app/javascript/components/order/Important.vue
+++ app/javascript/components/order/Important.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="tab">
     注意事項確認画面
+    <button @click="next" class="btn btn-outline-primary">次へ</button>
+    <button @click="previous" class="btn btn-outline-primary">前へ</button>
   </div>
 </template>
+
+<script>
+export default {
+  // props, data, methods, etc.
+  methods: {
+    next() {
+      this.$emit('change-page', {step: 1}); // 1ページ進む
+    },
+    previous(){
+      this.$emit('change-page', {step: -1}); // 1ページ戻る
+    }
+  }
+}
+</script>
