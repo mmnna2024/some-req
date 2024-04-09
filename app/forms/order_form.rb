841b316f2e6df6242265878513b392a848e5be80
naomu <muramatsu2023@gmail.com>
2024-04-09 16:16:43 +0900

    add:display and can update items price

--- app/forms/order_form.rb
+++ app/forms/order_form.rb
@@ -1,13 +1,23 @@
 class OrderForm
   include ActiveModel::Model
   include ActiveRecord::AttributeAssignment
-  attr_accessor :category_ids, :customer_name, :customer_email, :customer_phonenumber, 
-                :customer_address, :customer_age, :customer_sex, :shipping_id, :order_note, :status, :channel, :ordered_on
+  attr_accessor :id, :category_ids, :customer_name, :customer_email, :customer_phonenumber,
+                :customer_address, :customer_age, :customer_sex, :shipping_id, :order_note, :status, :channel, :ordered_on, :items, :url
 
   def initialize(attributes = nil, order: Order.new)
     @order = order
     attributes ||= default_attributes
     super(attributes)
+  end
+
+  def items_attributes=(attributes)
+    @items = attributes.map do |i, attribute|
+      Item.new(attribute)
+    end
+  end
+
+  def set_url(url)
+    @url = url
   end
 
   def save
@@ -20,17 +30,25 @@ class OrderForm
       end
 
       if @order.persisted?
-        @order.update!(note: order_note, shipping_id: shipping_id, ordered_on: ordered_on, status: status, channel: channel)
+        @order.update!(note: order_note, shipping_id: shipping_id, status: status, channel: channel)
+        @order.items.each_with_index do |item, index|
+          category_id = category_ids[index]
+          price = @items[index].price 
+          item.update!(order_id: order.id, category_id: category_id, price: price)
+        end
       else
         @order = Order.create!(note: order_note, customer_id: customer.id, shipping_id: shipping_id, ordered_on: Time.current, status: status, channel: channel)
-      end
-
-      @order.items.destroy_all
-      category_ids.each do |category_id|
-        category = Category.find_by_id(category_id)
-        if category
-          item = Item.create!(order_id: order.id, category_id: category.id, price: category.price)
+        total_order_price = 0
+        if category_ids.present?
+          category_ids.each do |category_id|
+            category = Category.find_by_id(category_id)
+            if category
+              item = Item.create!(order_id: order.id, category_id: category.id, price: category.price)
+              total_order_price += item.price
+            end
+          end
         end
+        @order.update!(price: total_order_price)
       end
     end
   end
@@ -55,6 +73,8 @@ class OrderForm
       status: @order.status,
       channel: @order.channel,
       category_ids: @order.items.pluck(:category_id),
+      items: @order.items,
+      id: @order.id,
     }
   end
 end
