cd389a0b3705e0ec17241ee52ac0f51009cc9463
kasagata <k.sagata.1207@gmail.com>
2024-04-19 18:42:54 +0900

    fix: select file extention

--- app/javascript/components/order/Form.vue
+++ app/javascript/components/order/Form.vue
@@ -26,7 +26,7 @@
               </select>
             </div>
             <div class="col-sm-5">
-              <input type="file" @change="selectedFile($event, v_index)" id="inputGroupFile01" name="products[image][]" accept="image/png, image/jpg" multiple>
+              <input type="file" @change="selectedFile($event, v_index)" id="inputGroupFile01" name="products[image][]" accept="image/png, image/jpeg" multiple>
             </div>
             <div class="col-sm-2 align-self-center d-flex justify-content-center">{{ selected[v_index].price }} </div>
           </div>
