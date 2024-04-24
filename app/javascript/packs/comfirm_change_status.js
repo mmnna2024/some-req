6cd55d8ef016ef551233d73e8a64a87ed0b1ae91
naomu <muramatsu2023@gmail.com>
2024-04-24 11:06:45 +0900

    fix:errors in other pages

--- app/javascript/packs/comfirm_change_status.js
+++ app/javascript/packs/comfirm_change_status.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const radio_button = document.getElementById('confirmable_radio_button');
-  radio_button.addEventListener('change', function() {
+  const radioButton = document.getElementById('confirmable_radio_button');
+  if (!radioButton){ return false;}
+  radioButton.addEventListener('change', function() {
     if(this.checked) {
       window.confirm('『返送済』に変更後、『更新する』を押すと発送完了メールが自動で送信されます。')
     }
